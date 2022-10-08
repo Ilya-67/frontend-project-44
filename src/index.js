0252d267e4f2f540e39b40f92d9fab269ec1dc9f
Ilya Zelnikov <asv67@seznam.cz>
2022-10-08 02:26:58 +0200

    update greeting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export function getGreetings() {
-    console.log('Welcom to the Brain Games!');
+    console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('May I have your name?: ');
     console.log(`Hello, ${userName}!`);
     return userName;
