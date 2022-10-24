8deec66168318fdbc573d73cd60877d18bd86b65
Ilya Zelnikov <asv67@seznam.cz>
2022-10-24 20:13:41 +0200

    fixed bug in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-export const getRandonInteger = (min, max) => {
+export default function getRandonInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
-export default getRandonInteger();
